65d00e3d59260695db9351d936d1a2a3a06de19b
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-09 19:56:27 +0900

    initial commit

--- api_3stepwallet/settings.py
+++ api_3stepwallet/settings.py
@@ -12,18 +12,19 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(i87d_fa&15zky2quvu_uy^+&t^xtee7x+k9lut4u)03ym5a&^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -78,11 +79,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_3stepwallet.wsgi.application'
 
 REST_FRAMEWORK = {
-# ここにViewにアクセスできるユーザーの定義
+    # ここにViewにアクセスできるユーザーの定義
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-# ログインの方法について
+    # ログインの方法について
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
@@ -97,10 +98,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
 }
 
 # Password validation
@@ -139,5 +137,6 @@ AUTH_USER_MODEL = 'api.User'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
